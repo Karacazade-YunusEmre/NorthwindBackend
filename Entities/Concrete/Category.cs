12d503ee2bc88b51d61c0cdb460af862537337c4
Karacazade-YunusEmre <karacazade.yunusemre@gmail.com>
2023-01-04 09:01:27 +0300

    Entity ler oluşturuldu

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Core.Entities;
+
+namespace Entities.Concrete;
+
+public class Category : IEntity
+{
+    [Required]
+    [DisplayName("Id")]
+    [Column(name: "CategoryID")]
+    public int Id { get; set; }
+
+    [Required]
+    [DisplayName("CategoryName")]
+    [Column(name: "CategoryName")]
+    public string Name { get; set; } = null!;
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Core.Entities;
+
+namespace Entities.Concrete;
+
+public class Product : IEntity
+{
+    [Required]
+    [DisplayName("Id")]
+    [Column(name: "ProductID")]
+    public int Id { get; set; }
+
+    [Required]
+    [DisplayName("CategoryId")]
+    [Column(name: "CategoryID")]
+    public int CategoryId { get; set; }
+
+    [Required]
+    [DisplayName("ProductName")]
+    [Column(name: "ProductName")]
+    public string Name { get; set; } = null!;
+
+    [DisplayName("QuantityPerUnit")]
+    [Column(name: "QuantityPerUnit")]
+    public string QuantityPerUnit { get; set; } = null!;
+
+    [DisplayName("UnitPrice")]
+    [Column(name: "UnitPrice")]
+    public decimal UnitPrice { get; set; }
+
+    [DisplayName("UnitsInStock")]
+    [Column(name: "UnitsInStock")]
+    public short UnitsInStock { get; set; }
+}
